ef4e8268cdfa7004d6e674b066916361fe51d878
anandsr724 <anandsharmaimn22@gmail.com>
2024-11-05 16:39:05 +0530

    model evaluation added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from equation_solver import logger
 from equation_solver.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from equation_solver.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from equation_solver.pipeline.stage_03_model_training import ModelTrainingPipeline
+from equation_solver.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 
@@ -34,3 +35,13 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Evaluation"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = EvaluationPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
--- src/equation_solver/config/configuration.py
+++ src/equation_solver/config/configuration.py
@@ -1,7 +1,9 @@
 
 from equation_solver.constants import *
 from equation_solver.utils.common import read_yaml, create_directories, save_json
-from equation_solver.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig,TrainingConfig
+from equation_solver.entity.config_entity import (DataIngestionConfig,
+                                                  PrepareBaseModelConfig,
+                                                  TrainingConfig,EvaluationConfig)
 import os
 
 class ConfigurationManager:
@@ -74,3 +76,14 @@ class ConfigurationManager:
         )
 
         return training_config
+        
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/symbol_data",
+            mlflow_uri="https://dagshub.com/anandsr724/Handwritten_Equation_Solver.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
