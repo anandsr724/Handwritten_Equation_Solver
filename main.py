45b28ac670578bc269c401be8b3178ca0c823063
anandsr724 <anandsharmaimn22@gmail.com>
2024-11-05 12:59:03 +0530

    Stage 3

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from equation_solver import logger
 from equation_solver.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from equation_solver.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from equation_solver.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 
 
@@ -18,6 +19,16 @@ STAGE_NAME = "Prepare Base Model"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = PrepareBaseModelTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
